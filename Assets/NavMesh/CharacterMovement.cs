9fe8ecf2744b68801f26acf3ed7e019dbeaa0366
Tasuku Takahashi <takahashitasuku@team-lab.com>
2023-03-08 18:47:15 +0900

    Update NavMesh Calculation

--- Assets/NavMesh/CharacterMovement.cs
+++ Assets/NavMesh/CharacterMovement.cs
@@ -5,20 +5,57 @@ using UnityEngine.AI;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private NavMeshAgent agent;
-    public Vector3 dest = new Vector3(3, 0, 0);
+    private Animator _animator;
+    private NavMeshAgent _agent;
+    public Vector3 _dest = new Vector3(3, 0, 0);
+    private float _forwardAmount = 0.5f;
 
     private void Awake()
     {
-        agent = this.GetComponent<NavMeshAgent>();
-        agent.updatePosition = true;
+        _agent = this.GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _animator.applyRootMotion = true; //applying position from animation
+        //_animator.applyRootMotion = false;
+        _animator.enabled = false;
+
+        _agent.updatePosition = true;
+        _agent.updateRotation = true;
+
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            agent.SetDestination(dest);
-        }    
+            _animator.enabled = true;
+            _animator.Play("Walk");
+        }
     }
+
+    private void OnAnimatorMove()
+    {
+        this.UpdatePosition();
+
+        //_agent.SetDestination(_dest);
+    }
+
+    private void FixedUpdate()
+    {
+        //_animator.SetFloat("Forward", _forwardAmount, 0.1f, Time.deltaTime);
+    }
+
+
+    private void UpdatePosition()
+    {
+        var position = _animator.rootPosition;
+
+        transform.position = position;
+
+        Vector3 worldDeltaPosition = _agent.nextPosition - transform.position;
+
+        if (worldDeltaPosition.magnitude > _agent.radius)
+            _agent.nextPosition = transform.position + 0.3f * worldDeltaPosition;
+    }
+
+
 }
